d77f895f003617d0077e939ed29ba3b1a645132c
DESKTOP-NFE8S1M\Isac <isac.walimaa@wiccon.se>
2022-06-02 16:21:18 +0200

    models

---
+++ GUICbit/Data/Customer.cs
@@ -0,0 +1,13 @@
+﻿namespace GUICbit.Data
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string? CustomerReference { get; set; }
+        public string? CustomerInternalGuid { get; set; }
+        public int CustomerKYCLevel { get; set; }
+        //public ScreeningResult? ScreenResult { get; set; }
+        //public Platform Platform { get; set; } = default!;
+       // public Country? Country { get; set; }
+    }
+}
---
+++ GUICbit/Data/Payout.cs
@@ -0,0 +1,10 @@
+﻿namespace GUICbit.Data
+{
+    public class Payout
+    {
+        public int Id { get; set; }
+        public double Ammount { get; set; }
+        public string PayIntId { get; set; }
+        public string Status { get; set; }
+    }
+}
