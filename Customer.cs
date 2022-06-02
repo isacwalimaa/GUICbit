d60dcd07b2f5f4948d8451343d1116516c9ade1f
DESKTOP-NFE8S1M\Isac <isac.walimaa@wiccon.se>
2022-06-02 16:11:12 +0200

    model

---
+++ Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Customer
+{
+    public Customer()
+    {
+
+    public int Id { get; set; }
+    public string? CustomerReference { get; set; }
+    public string? CustomerInternalGuid { get; set; }
+    public int CustomerKYCLevel { get; set; }
+    public string? CustomerInternalStatus { get; set; }
+    public string? CustomerInternalColor { get; set; }
+    public string? CustomerInternalComment { get; set; }
+    public ScreeningResultModel? ScreeningResult { get; set; }
+    public PlatformModel? Platform { get; set; }
+    public CountryModel? Country { get; set; }
+    public CustomerInformationModel? CustomerInformation { get; set; }
+    public virtual ICollection<BankAccountDetailModel>? BankAccountDetails { get; set; } = new List<BankAccountDetailModel>();
+}
+}
