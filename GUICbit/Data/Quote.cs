61114423d2b63b2312c0e83deedf1f524ead6a0b
Jonas Hestréus <jonas.hestreus@wiccon.se>
2022-06-02 16:14:40 +0200

    Made the Payin and Quote Model

---
+++ GUICbit/Data/Quote.cs
@@ -0,0 +1,13 @@
+﻿namespace GUICbit.Data
+{
+    public class Quote
+    {
+        public int Id { get; set; }
+        public string SettlementCurrency { get; set; }
+        public string CollectionCurrency { get; set; }
+        public double collectionAmount { get; set; }
+        public double SettlementAmount { get; set; }
+        public string Status { get; set; }
+        public string CustomerRef { get; set; }
+    }
+}
